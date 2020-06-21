9841951ce987169ce2f6c98fce087e8bb87a4be3
Zev Tovbin <Zev.tovbin@gmail.com>
2020-06-21 16:06:43 +0300

    Files

---
+++ Week 2/Day 1/XP-Gold Day 1/script.js
@@ -0,0 +1,36 @@
+let number1 = prompt("A number")
+let a = parseInt(number1)
+let number2 = prompt("Another Number")
+let b = parseInt(number2)
+alert(a + b) 
+
+
+let number3 = prompt("A number")
+let c = parseInt(number3)
+let number4 = prompt("Another Number")
+let d = parseInt(number4)
+alert(c * d) 
+
+
+
+let number5 = prompt("A number")
+let e = parseInt(number5)
+let number6 = prompt("Another Number")
+let f = parseInt(number6)
+alert(e / f)  
+
+
+let number7 = prompt("A number")
+let g = parseInt(number1)
+let number8 = prompt("Another Number")
+let h = parseInt(number2)
+alert(g - h) 
+
+
+
+
+
+
+
+ 
+ 
