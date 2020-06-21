601e44f458f6737f6728c235f40f487bb0a3de34
Zev Tovbin <Zev.tovbin@gmail.com>
2020-06-21 14:39:36 +0300

    Files

---
+++ Coding Files/Week 2/Daily Challanges/DailyChallange Day 1 /script.js
@@ -0,0 +1,18 @@
+let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+fruits.splice(0,1) 
+fruits.sort()
+fruits.push("Kiwi")
+fruits.splice(0,1)
+fruits.reverse()
+console.log(fruits) 
+
+
+
+
+
+let fruits2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+
+
+
+console.log(fruits2[1][1])
+
