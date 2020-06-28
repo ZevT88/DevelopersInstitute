c23d8dd2b2002fb0c428bc03d626ac279d0b5dfb
Zev Tovbin <Zev.tovbin@gmail.com>
2020-06-28 22:48:53 +0300

    Week 3 Day 1

---
+++ Week 3/Day 1/DailyChallange Day 1/script.js
@@ -0,0 +1,25 @@
+
+let array = [ 
+    
+
+
+
+    book1 = { 
+        title : "The Magicians",
+        auther : "Lev Grossman",
+        image : "", 
+        alreadyRead : true
+    },
+
+    book2 = { 
+        title : "Harry Potter and The Sorcerer's Stone",
+        auther : "J.K Rowling",
+        image : "",
+        alreadyRead : true
+    }
+
+ 
+] 
+
+
+
