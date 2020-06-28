92674e85f5b19e69feb01a643e245cba3ccf119b
Zev Tovbin <Zev.tovbin@gmail.com>
2020-06-28 19:29:29 +0300

    files

---
+++ Week 3/Day 1/ExerciseXP Day 1 /script.js
@@ -0,0 +1,16 @@
+// Exercise #1 
+
+// function myFunction() {
+//     document.getElementsByTagName("div")[0].setAttribute("class", "socialNetworkNavigation"); 
+//   }
+
+//Exercise #2 
+
+// function myFunction2() {
+//     document.getElementsByTagName("ul")[0].children[1].setAttribute("Pete", "Richard")
+// }
+
+// console.log(document.getElementsByTagName("ul")[0].children[1].lastChild) 
+
+
+
