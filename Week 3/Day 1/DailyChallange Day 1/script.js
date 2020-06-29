dda36fdf755bbe012fab69b90feb74d07f3fc05c
Zev Tovbin <Zev.tovbin@gmail.com>
2020-06-29 20:50:17 +0300

    files

--- Week 3/Day 1/DailyChallange Day 1/script.js
+++ Week 3/Day 1/DailyChallange Day 1/script.js
@@ -1,23 +1,28 @@
-let img = document.createElement("IMG")
-let array = [
-
+let allBooks = [
+    
 
     book1 = { 
-        title : "The Magicians",
-        auther : "Lev Grossman",
-        image : document.getElementsByName("img"),
+        title : " The Magicians",
+        auther : "Written by Lev Grossman",
+        image : "book1.jpeg",
         alreadyRead : true
     },
 
     book2 = { 
         title : "Harry Potter and The Sorcerer's Stone",
-        auther : "J.K Rowling",
-        image : "",
+        auther : "Written by J.K Rowling",
+        image : "book2.jpeg",
         alreadyRead : true
     }
 
- 
 ] 
 
 
+let tab_table = document.createElement("table")
+let tabtr = document.createTextNode("tr").innerHTML = "title"
+let tab_td1 = document.createElement("td")
+let tab_td2 = document.createElement("td")
+let tab_td3 = document.createElement("td")
+let tab_td4 = document.createElement("td") 
 
+tab_table.appendChild(tabtr)
---
+++ Week 3/Day 2/ExerciseXP Day 2/script.js
@@ -0,0 +1,29 @@
+// // Exercise #1
+// document.children[0].lastChild.children[0].classList.add("para_article")
+
+// document.children[0].lastChild.children[0].children[5].remove() 
+
+// let h2 = document.children[0].children[1].children[0].children[1]
+// console.log(h2)  
+
+// h2.addEventListener("click", dlt)
+
+// function dlt() { 
+//     document.children[0].children[1].children[0].children[1].remove()
+// } 
+ 
+// let h1 = document.children[0].children[1].children[0].children[0]
+
+// let x = Math.floor(Math.random() * 101)
+// document.children[0].children[0].children[0].innerHTML =  `h1 { font-size: ${x}px;} `  
+
+// function dlt2() { 
+//     document.children[0].lastChild.children[0].children[2].remove()
+// }  
+
+// let p1 = document.children[0].lastChild.children[0].children[2]
+
+// p1.addEventListener("click", dlt2)
+
+
+
