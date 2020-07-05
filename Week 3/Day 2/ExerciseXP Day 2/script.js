0474c6e08c10a93b470b746afea3698e4e28fabc
Zev Tovbin <Zev.tovbin@gmail.com>
2020-07-05 09:26:51 +0300

    edits

--- Week 3/Day 2/ExerciseXP Day 2/script.js
+++ Week 3/Day 2/ExerciseXP Day 2/script.js
@@ -1,10 +1,9 @@
-// // Exercise #1
+// // // Exercise #1
 // document.children[0].lastChild.children[0].classList.add("para_article")
 
 // document.children[0].lastChild.children[0].children[5].remove() 
 
 // let h2 = document.children[0].children[1].children[0].children[1]
-// console.log(h2)  
 
 // h2.addEventListener("click", dlt)
 
@@ -14,8 +13,9 @@
  
 // let h1 = document.children[0].children[1].children[0].children[0]
 
-// let x = Math.floor(Math.random() * 101)
-// document.children[0].children[0].children[0].innerHTML =  `h1 { font-size: ${x}px;} `  
+let x = Math.floor(Math.random() * 101) 
+let y = Math.floor(Math.random() * 1000001) 
+document.children[0].children[0].children[0].innerHTML =  `h1 { font-size: ${x}px; color: #${y};} `  
 
 // function dlt2() { 
 //     document.children[0].lastChild.children[0].children[2].remove()
@@ -23,7 +23,25 @@
 
 // let p1 = document.children[0].lastChild.children[0].children[2]
 
-// p1.addEventListener("click", dlt2)
+// p1.addEventListener("click", dlt2) 
 
 
+// function fadeOutEffect() {
+//     var fadeTarget = document.children[0].lastChild.children[0].children[3]
+//     var fadeEffect = setInterval(function () {
+//         if (!fadeTarget.style.opacity) {
+//             fadeTarget.style.opacity = 1;
+//         }
+//         if (fadeTarget.style.opacity > 0) {
+//             fadeTarget.style.opacity -= 0.1;
+//         } else {
+//             clearInterval(fadeEffect);
+//         }
+//     }, 200);
+// }
 
+
+// let p2 = document.children[0].lastChild.children[0].children[3] 
+// p2.addEventListener("click", fadeOutEffect) 
+
+console.log(Math.floor(Math.random() * 1000001))
