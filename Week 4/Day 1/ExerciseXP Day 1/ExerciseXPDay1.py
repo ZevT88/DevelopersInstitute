f35f42a225efa5957ed3f8e3713ca27a97c0c0e6
Zev Tovbin <Zev.tovbin@gmail.com>
2020-07-05 12:56:50 +0300

    ExerciseXP Week 4 Day 1

---
+++ Week 4/Day 1/ExerciseXP Day 1/ExerciseXPDay1.py
@@ -0,0 +1,42 @@
+# Exercise #1
+# print("Hello World \n" * 5) 
+
+#Exercise #2  
+# print((99^3) * 8)  
+
+
+#Exercise #3
+# name = "Zev" 
+# age = 18
+# shoe_size = "Size 20 Mens" 
+# info = "My name is {}, I am {} years old, and my shoe size is {}".format(name, age, shoe_size)
+# print(info) 
+
+#Exercise #4 
+# computer_brand = "Apple" 
+# print("I have an {} computor".format(computer_brand)) 
+
+#Exercise #5  
+#False
+#True 
+#False
+#False 
+
+#Exercise #6 
+
+# height = int(input("what is your height in inches?"))
+
+# if height < 35: 
+#   print("You are too short sorry")
+# else: 
+#   print("You can Ride") 
+
+
+#Exercise #7 
+
+# num  = int(input("A number")) 
+# if num % 2 == 0: 
+#   print("Even") 
+# else: 
+#   print("Odd")
+
