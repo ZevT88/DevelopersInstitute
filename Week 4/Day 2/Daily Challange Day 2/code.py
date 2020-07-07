4aa8ea248a1a353092791f5462174bd2187badcb
Zev Tovbin <Zev.tovbin@gmail.com>
2020-07-07 11:41:53 +0300

    edits

--- Week 4/Day 2/Daily Challange Day 2/code.py
+++ Week 4/Day 2/Daily Challange Day 2/code.py
@@ -7,7 +7,6 @@ month1 = int(input("What is the Month you where born?  "))
 day1 = int(input("What is the Day you where born?  "))
 mytuple =  (year1, month1,day1)
 a, b, c = mytuple
-
 def calculateAge(birthDate): 
     today = date.today() 
     age = today.year - birthDate.year - ((today.month, today.day) < (birthDate.month, birthDate.day)) 
@@ -15,6 +14,14 @@ def calculateAge(birthDate):
     return age 
 
 age1 = [(calculateAge(date(a, b, c)))] 
-cypher = str(age1)[2]
+cypher = int(str(age1)[2])
 b = "i" *  int(cypher)
-print(f"        ___{b}___\n       |:H:a:p:p:y:| \n     __|___________|__ \n    |^^^^^^^^^^^^^^^^^|\n    |:B:i:r:t:h:d:a:y:| \n    |                 | \n    ~~~~~~~~~~~~~~~~~~~" ) 
+side_cake = int((11 - cypher) / 2) * "_"
+
+print(f"    {side_cake}{b}{side_cake}")
+print("   |:H:A:P:P:Y:|")
+print(" __|___________|__")
+print("|^^^^^^^^^^^^^^^^^|")
+print("|:B:I:R:T:H:D:a:Y:|")
+print("|                 |")
+print("~~~~~~~~~~~~~~~~~~~")
