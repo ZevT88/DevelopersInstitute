e04bcac626578f918a3c0e4037f3420f22a20674
Zev Tovbin <Zev.tovbin@gmail.com>
2020-07-10 16:56:54 +0300

    week3 1

--- Week 3/Day 1/DailyChallange Day 1/script.js
+++ Week 3/Day 1/DailyChallange Day 1/script.js
@@ -1,28 +1,56 @@
-let allBooks = [
-    
-
-    book1 = { 
-        title : " The Magicians",
-        auther : "Written by Lev Grossman",
-        image : "book1.jpeg",
-        alreadyRead : true
+let all_books = [
+    book1 = {
+        title: "Harry Potter",
+        auther: "J.K Rolling",
+        image: "Harry_Potter.jpg",
+        already_read: true
     },
 
-    book2 = { 
-        title : "Harry Potter and The Sorcerer's Stone",
-        auther : "Written by J.K Rowling",
-        image : "book2.jpeg",
-        alreadyRead : true
+    book2 = {
+        title: "The Hobbit",
+        auther: "J.R.R Tolkien",
+        image: "The_Hobbit.jpg",
+        already_read: true
     }
-
-] 
-
+]
 
 let tab_table = document.createElement("table")
-let tabtr = document.createTextNode("tr").innerHTML = "title"
-let tab_td1 = document.createElement("td")
-let tab_td2 = document.createElement("td")
-let tab_td3 = document.createElement("td")
-let tab_td4 = document.createElement("td") 
-
-tab_table.appendChild(tabtr)
+for (let book in all_books) {
+    let tab_tr = document.createElement("tr")
+    for (let i = 0; i <= 3; i++) {
+        let tab_td = document.createElement("td")
+        tab_td.style.paddingRight = "20px"
+        let text = ""
+        let image
+        if (i == 0) {
+            text = document.createElement("h3")
+            text.innerHTML = all_books[book].title
+            if (all_books[book].already_read == true) {
+                text.style.color = "red"
+            }
+            tab_td.appendChild(text)
+        } else if (i == 1) {
+            text = document.createElement("h3")
+            text.innerHTML = "written by:"
+            if (all_books[book].already_read == true) {
+                text.style.color = "red"
+            }
+            tab_td.appendChild(text)
+        } else if (i == 2) {
+            text = document.createElement("h3")
+            text.innerHTML = all_books[book].auther
+            if (all_books[book].already_read == true) {
+                text.style.color = "red"
+            }
+            tab_td.appendChild(text)
+        } else {
+            image = document.createElement("img")
+            image.setAttribute("src", all_books[book].image)
+            image.style.width = "100px"
+            tab_td.appendChild(image)
+        }
+        tab_tr.appendChild(tab_td)
+        tab_table.appendChild(tab_tr)
+    }
+}
+document.body.appendChild(tab_table)
