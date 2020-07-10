0526468e4fbf0dfea703d5866943c4ff9dfbe265
Zev Tovbin <Zev.tovbin@gmail.com>
2020-07-10 18:13:59 +0300

    week 3 day 3

---
+++ Week 3/Day 3/DailyChallange Day 3/script.js
@@ -0,0 +1,8 @@
+function letters() {
+    let box = document.getElementsByTagName("input")[0];
+    let word = box.value;
+    let letter = word.substr(-1, 1);
+    if (parseInt(letter)) {
+        box.value = word.substring(0, word.length - 1);
+    }
+}
