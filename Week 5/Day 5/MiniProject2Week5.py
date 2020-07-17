7a7e7260eeb290dc7bc7c9ef7868008d1e3a61ce
Zev Tovbin <Zev.tovbin@gmail.com>
2020-07-17 12:05:18 +0300

    files

---
+++ Week 5/Day 5/MiniProject2Week5.py
@@ -0,0 +1,40 @@
+def all_perms(elements):
+    if len(elements) <=1:
+        yield elements
+    else:
+        for perm in all_perms(elements[1:]):
+            for i in range(len(elements)):
+                yield perm[:i] + elements[0:1] + perm[i:]
+
+class AnagramChecker(): 
+    def __init__(self): 
+        self.allwords = [] 
+        for line in open('Anagrams.txt'):
+            self.allwords.append(line.strip()) 
+
+
+    def is_valid_word(self,word): 
+        if word in self.allwords: 
+            print("This is a valid English word")
+        else: 
+            print("This is not a valid English word")
+
+    def get_anagrams(self, word):  
+        anagrams = []
+        word_perms = list(all_perms(f"{word}"))  
+
+        for perm in word_perms: 
+            if perm in self.allwords: 
+                anagrams.append(perm) 
+        
+        anagrams.pop(0)
+        
+        print(f'YOUR WORD: "{word}"')
+        anagram.is_valid_word(word) 
+        print(f"Anagrams for your word: {', '.join(anagrams)}")
+
+
+  
+
+anagram = AnagramChecker() 
+anagram.get_anagrams("MEAT") 
