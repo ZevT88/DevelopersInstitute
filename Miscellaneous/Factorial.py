61119f97fd6c25fb079772bb95a6694b64a7b4a2
Zev Tovbin <Zev.tovbin@gmail.com>
2020-08-31 09:12:13 +0300

    FinalProject

--- Miscellaneous/Factorial.py
+++ Miscellaneous/Factorial.py
@@ -1,22 +1,14 @@
-from termcolor import colored
-def Factorial(input_number):
-    input_num = input_number
-    num = input_num - 1 
-    for i in range(num): 
-        output = input_num * (num - i) 
-        # time = colored(f"times {num - i} is", 'red')
-        equals = colored("=", 'red')
-        # print(f"{input_num:,d} {time} {output:,d}" )
-        input_num = output 
-    print(f"{input_number}! {equals} {input_num:,d}")
+input_num = int(input("Input a Number: "))
+num = input_num - 1 
+for i in range(num): 
+    output = input_num * (num - i)
+    print(f"{input_num} times {num - i} is {output}" )
+    input_num = output 
+    
 
 
 
 
-
-for i in range(1,100): 
-    num = i + 1
-    Factorial(num)
 
 
 
