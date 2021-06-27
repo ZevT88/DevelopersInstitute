8e1ead1bc8c43eaa94c907542bd530666a5170c0
ZevT88 <65665740+ZevT88@users.noreply.github.com>
2021-06-27 09:34:47 +0300

    Delete Week 2 directory

--- Week 2/Day 1/ExerciseXP Day 1 /script.js
+++
@@ -1,9 +0,0 @@
-let str = ["Dory", "Dory", "Dory", "Dory", "Nemo", "Dory", "Dory"]
-for (i of str) { 
-   console.log(`I found Nemo at spot ${ str.indexOf("Nemo") + 1}`)  
-   
-}
-   
-
-
-
--- Week 2/Day 4/DailyChallange Day 4/script.js
+++
@@ -1,8 +0,0 @@
-
-function inaFrame (word1, word2, word3, word4, word5){ 
-        
-    alert("**********" + "\n" + "*   " +  (word1 ) + "   *" + "\n" + "*   " +  (word2 ) + "  *" + "\n" + "*      " +  (word3 ) + "      *" + "\n" + "*      " +  (word4 ) + "       *" + "\n" + "*  " +  (word5 ) + "   *" ) 
-
-
-} 
-inaFrame("hello", "world", "in", "a", "frame")
