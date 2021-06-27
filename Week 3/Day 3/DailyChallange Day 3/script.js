b70a869171d18267ce6ce45defb53ecf33d369c0
ZevT88 <65665740+ZevT88@users.noreply.github.com>
2021-06-27 09:34:56 +0300

    Delete Week 3 directory

--- Week 3/Day 3/DailyChallange Day 3/script.js
+++
@@ -1,8 +0,0 @@
-function letters() {
-    let box = document.getElementsByTagName("input")[0];
-    let word = box.value;
-    let letter = word.substr(-1, 1);
-    if (parseInt(letter)) {
-        box.value = word.substring(0, word.length - 1);
-    }
-}
